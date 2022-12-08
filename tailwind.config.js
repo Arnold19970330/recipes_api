1b86318312c2e90888435c906b3b1f09db2fd5d1
Galaczi Arnold <tinkodev@gmail.com>
2022-12-08 20:32:16 +0200

    tailwindconfig changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html"],
+  content: ["*.{html,js}"],
   theme: {
     backgroundImage: {
-      'page-face': "url('/img/background-image.webp')",
-      'page-face1': "url('/img/bg-2.jpg')"
+      'page-face': "url('img/background-image.webp')",
+      'page-face1': "url('img/bg-2.jpg')"
     }
   },
   plugins: [],
 }
+
